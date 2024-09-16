8436dc2fafec2771016e5fe9c03e29504b4b75be
ChristianBingman <christianbingman@gmail.com>
2024-09-15 20:48:59 -0500

    Add cert-manager

--- init.tf
+++ init.tf
@@ -26,3 +26,35 @@ resource "helm_release" "metrics_server" {
     value = "--kubelet-insecure-tls"
   }
 }
+
+resource "helm_release" "cert-manager" {
+  name = "cert-manager"
+  repository = "https://charts.jetstack.io"
+  chart = "cert-manager"
+  namespace = "cert-manager"
+  create_namespace = true
+  version = "v1.15.0"
+
+  set {
+    name = "crds.enabled"
+    value = true
+  }
+
+  set_list {
+    name = "extraArgs"
+    value = [
+      "--dns01-recursive-nameservers-only",
+      "--dns01-recursive-nameservers=8.8.8.8:53,1.1.1.1:53"
+    ]
+  }
+
+  set {
+    name = "prometheus.enabled"
+    value = true
+  }
+
+  set {
+    name = "prometheus.servicemonitor.enabled"
+    value = true
+  }
+}
