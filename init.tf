06140ab75bb247fe51216d643afad8f810036f9f
ChristianBingman <christianbingman@gmail.com>
2024-09-17 20:05:37 -0500

    Add cnpg helm chart

--- init.tf
+++ init.tf
@@ -58,3 +58,21 @@ resource "helm_release" "cert-manager" {
     value = true
   }
 }
+
+resource "helm_release" "cnpg" {
+  name = "cnpg"
+  repository = "https://cloudnative-pg.github.io/charts"
+  chart = "cloudnative-pg"
+  namespace = "cnpg-system"
+  create_namespace = true
+
+  set {
+    name = "monitoring.podMonitorEnabled"
+    value = true
+  }
+
+  set {
+    name = "monitoring.grafanaDashboard.create"
+    value = true
+  }
+}
